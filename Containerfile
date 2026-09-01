8a38a6d6b71f1a3f3802f3c40f666fa54882696d
gergoszaszvaradi <szaszvaradi.gergo@yahoo.com>
2026-09-01 20:46:32 +0300

    first commit

---
+++ Containerfile
@@ -0,0 +1,41 @@
+FROM quay.io/fedora/fedora-bootc:44
+
+ARG HOSTNAME=wacky
+ARG TIMEZONE=UTC
+
+# Set hostname
+RUN echo "$HOSTNAME"            > /etc/hostname && \
+	echo "127.0.0.1	$HOSTNAME" >> /etc/hosts && \
+	echo "::1		$HOSTNAME" >> /etc/hosts
+
+# Set timezone
+RUN ln -snf "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime && \
+    echo "${TZ}" > /etc/timezone
+
+# Install other tools
+RUN dnf install -y \
+        git \
+        wget \
+        curl \
+        htop \
+        jq \
+        yq \
+        openssh-server \
+    && dnf clean all
+
+# Setup SSH
+RUN printf '%s\n' \
+        'PubkeyAuthentication yes' \
+        'PasswordAuthentication no' \
+        'KbdInteractiveAuthentication no' \
+    >> /etc/ssh/sshd_config
+
+# Setup firewall
+RUN firewall-offline-cmd --add-service=ssh
+
+# Enable systemd services
+RUN systemctl enable sshd.service && \
+    systemctl enable firewalld.service
+
+# Validate the container
+RUN bootc container lint
---
+++ scripts/build-installer.sh
@@ -0,0 +1,36 @@
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+mkdir -p .output
+
+read -p "Is ./installer/config.toml updated? [Ny]: " yn
+case $yn in
+    [Yy]* ) break;;
+    * ) exit;;
+esac
+
+podman build -t localhost/wacky-homes-installer:latest -f ./installer/Containerfile .
+
+podman run \
+    --rm \
+    -it \
+    --privileged \
+    --pull=newer \
+    -v /var/lib/containers/storage:/var/lib/containers/storage \
+    -v ./installer/config.toml:/config.toml:ro \
+    -v ./.output:/output \
+    quay.io/centos-bootc/bootc-image-builder:latest \
+    --type bootc-installer \
+    --rootfs xfs \
+    --installer-payload-ref ghcr.io/wacky-homes/server:latest \
+    localhost/wacky-homes-installer:latest
+
+echo ""
+if [ -e .output/bootiso/installer.iso ]; then
+    echo "ISO ready at .output/bootiso/installer.iso"
+    echo "Flash it to a USB drive with: sudo dd if=.output/bootiso/installer.iso of=/dev/sdX bs=8M status=progress"
+else
+    echo "Failed to build installer"
+fi
